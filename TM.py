b5ac9fc010e5df6ef3169a794f45098c9c54425d
Scorptar <Scorptar@gmail.com>
2021-03-26 12:16:28 +0100

    User experience done
    Completely safe and intuitive to use. Stable version tkinter ok

--- TM.py
+++ TM.py
@@ -1,4 +1,4 @@
-    # Created By Nicolas Stenuit
+# Created By Nicolas Stenuit
 # Reviewed By Lucas Garcia
 # Version 3.0 = 21/05
 #########################################
@@ -17,6 +17,9 @@ import os
 import sys
 import glob
 import serial
+import time
+
+
 #########################################
 # Definition de nos variables globales
 def serial_ports():
@@ -46,22 +49,28 @@ def serial_ports():
         except (OSError, serial.SerialException):
             pass
     return result[0]
-COM=serial_ports()
+
+
+COM = serial_ports()
 Fe = 100
-T = 1.0 / Fe #sample spacing
+T = 1.0 / Fe  # sample spacing
 baudrate = 115200
 datas = []
 ser = serial.Serial(COM, baudrate, timeout=1)
 firstclick = True
+loading_ok = False
 rapport = False
-nbcapteur = 1 #nombre d'accéléromètres employés
+starttime = 0
+nbcapteur = 1  # nombre d'accéléromètres employés
+
+
 ######################################### OK !
 def rapport_gen(nom, prenom, n_date):
     # fonction servant à générer un pdf servant de rapport pour le client.
     global rapport
-    #print(rapport)
-    if rapport == True:   # si l'analyse à été réalisée -> on génère le pdf
-        #print("pdf généré")
+    # print(rapport)
+    if rapport == True:  # si l'analyse à été réalisée -> on génère le pdf
+        # print("pdf généré")
         nom_ln = str("Nom : " + nom)
         prenom_ln = str("Prenom : " + prenom)
         ndate = n_date.format('%d/%m/%Y')
@@ -69,28 +78,33 @@ def rapport_gen(nom, prenom, n_date):
         auj = str("Date de mesure : " + str(datetime.now().strftime("%d/%m/%Y : %H:%M")))
         # variable servant à identifier le patient
 
-    # Config PDF generator
+        # Config PDF generator
         pdf = FPDF(orientation='P', unit='mm', format='A4')
         pdf.add_page()
         pdf.set_font("Arial", size=12)
 
-    # Textes
+        # Textes
         pdf.text(10, 10, prenom_ln)
         pdf.text(10, 15, nom_ln)
         pdf.text(10, 20, ndate_ln)
         pdf.text(130, 10, auj)
 
-    #Images
-    #Capt 1
+        # Images
+        # Capt 1
         pdf.image('Images/Analyse-capt-1X.png', x=0, y=30, w=200)
         pdf.image('Images/Analyse-capt-1Y.png', x=0, y=110, w=200)
         pdf.image('Images/Analyse-capt-1Z.png', x=0, y=190, w=200)
 
         pdf.output("Rapport_" + nom + "_" + prenom + ".pdf")
-        rapport = False # reset de la valeur pour analyse future
-    #Open PDF
+        rapport = False  # reset de la valeur pour analyse future
+        # Open PDF
         os.startfile("Rapport_" + nom + "_" + prenom + ".pdf")
-######################################### OK !
+        info_label['text'] = "Rapport généré. Celui-ci s'ouvrira automatiquement dans quelques instants. "
+        fenetre.update_idletasks()
+
+    ######################################### OK !
+
+
 def define_value(choix):
     # fonction permettant de définir les constantes du nombres d'époque et du nombre de ligne de données attendue par le
     # microcontrolleur
@@ -124,70 +138,81 @@ def define_value(choix):
         else:
             print("Veuillez rentrer un choix valide")
     return Nb_ligne, Nb_ech
-######################################### OK
-def read_serial(Nb_ligne):
-    #fonction permettant de lire et de selectionner l'information envoyée sur le port série par le controlleur
-    for i in range(0, Nb_ligne+30*(Nb_ligne//3000)):
-        line = ser.readline()    #lit la ligne envoyée sur le port série et stocke les valeurs dans un string.
-        if line:
-            reg = re.search(r'\'(.*?)\\', str(line)) # Regex permettant d'isoler l'information nécessaire proprement.
-            data = str(reg.group(1))  # stocke l'information mis en evidence par la regex dans une variable
-            data = data.split(',')    # segmente la variable en une liste dont chaque cellule correspond a un axe
-            datas.append(data)       # ajoute la liste précédente a la fin d'une matrice
-            print(line)
-        #print(round(100 * i / Nb_ligne))  # indique le pourcentage de completion de l'analyse
-        if (round(100 * i / Nb_ligne) != progress['value'] and round(100 * i / Nb_ligne)%5==0): #limite à 100 le nb de rafraichissement de la loading bar
-            progress['value'] = round(100 * i / Nb_ligne)
-            fenetre.update_idletasks()
-    return datas
+
 
 ######################################### OK
-def get_axis_value(Axe,data_List,Nb_ligne,):
+def read_serial(Nb_ligne):
+    # fonction permettant de lire et de selectionner l'information envoyée sur le port série par le controlleur
+    for i in range(0, Nb_ligne + 30 * (Nb_ligne // 3000)):
+        line = ser.readline()  # lit la ligne envoyée sur le port série et stocke les valeurs dans un string
+        if i == 30 * (Nb_ligne // 3000) - 1:
+            info_label['text'] = "Prise de mesure terminée. Transfert vers PC en cours"
+            info2_label['text'] = "Temps estimé : {} secondes.".format(8 * (Nb_ligne // 3000))
+            fenetre.update_idletasks()
+            print("j'ai écris")
+        if line:
+            reg = re.search(r'\'(.*?)\\', str(line))  # Regex permettant d'isoler l'information nécessaire proprement.
+            data = str(reg.group(1))  # stocke l'information mis en evidence par la regex dans une variable
+            data = data.split(',')  # segmente la variable en une liste dont chaque cellule correspond a un axe
+            datas.append(data)  # ajoute la liste précédente a la fin d'une matrice
+            print(line)
+        # print(round(100 * i / Nb_ligne))  # indique le pourcentage de completion de l'analyse
+    info_label['text'] = "Données reçues. Vous pouvez imprimer le rapport."
+    info2_label['text'] = "                                                      "
+    return datas
+
+
+######################################### OK
+def get_axis_value(Axe, data_List, Nb_ligne, ):
     # fonction permettant d'isoler les données de chaque axe dans des listes séparées
     List = []
-    for i in range(0,Nb_ligne):
-        ech =float(data_List[i][Axe])
+    for i in range(0, Nb_ligne):
+        ech = float(data_List[i][Axe])
         List.append(ech)
     return List
+
+
 ######################################### MODIF
-def plot(k,Nb_ligne,Data_axis,axis_split):
+def plot(k, Nb_ligne, Data_axis, axis_split):
     # fonction permettant l'analyse fréquentielle ainsi que la génération de graphiques
 
     # définition de l'axe
-    if k % (3*nbcapteur) == 0:
+    if k % (3 * nbcapteur) == 0:
         Axe = str("X")
-    elif k % (3*nbcapteur) == 1:
+    elif k % (3 * nbcapteur) == 1:
         Axe = str("Y")
     else:
         Axe = str("Z")
 
     ffts = []
-    t = np.linspace(0.0, Nb_ligne * T, Nb_ligne) # definition de notre vecteur temporel allant de 0 jusqu'a la valeur
-                                                 # définie par l'utilisateur
-    xf = np.linspace(0.0,30,2*Fe)                 # vecteur fréquentiel
-    for element in range(len(axis_split)):       # pour chaque époque -> générer une fft
-        yf = scipy.fftpack.fft(axis_split[element]) # genere la fft
-        ffts.append(np.abs(yf))                     # nous n'avons besoin de que la valeur absolue de la fft
-    somme = [sum(row[i] for row in ffts) for i in range(len(ffts[0]))] # calcul une somme pour chaque i eme valeurs des époques
-    moy =[x /len(ffts) for x in somme]                                 # division par le nombre d'époque pour avoir une moyenne
-    MOY = np.asarray(moy)                                              # utilisation de numpy pour un meilleur affichage
+    t = np.linspace(0.0, Nb_ligne * T, Nb_ligne)  # definition de notre vecteur temporel allant de 0 jusqu'a la valeur
+    # définie par l'utilisateur
+    xf = np.linspace(0.0, 30, 2 * Fe)  # vecteur fréquentiel
+    for element in range(len(axis_split)):  # pour chaque époque -> générer une fft
+        yf = scipy.fftpack.fft(axis_split[element])  # genere la fft
+        ffts.append(np.abs(yf))  # nous n'avons besoin de que la valeur absolue de la fft
+    somme = [sum(row[i] for row in ffts) for i in
+             range(len(ffts[0]))]  # calcul une somme pour chaque i eme valeurs des époques
+    moy = [x / len(ffts) for x in somme]  # division par le nombre d'époque pour avoir une moyenne
+    MOY = np.asarray(moy)  # utilisation de numpy pour un meilleur affichage
 
-
-    plt.figure(figsize=(12,4.8))     # paramètre pour générer des graphiques
+    plt.figure(figsize=(12, 4.8))  # paramètre pour générer des graphiques
     plt.subplot(211)  # subplot pour le graphique temporel
     plt.title("Évolution temporelle & fréquentielle du capteur  - Axe : " + Axe)
-    plt.plot(t,Data_axis,linewidth=0.5)
+    plt.plot(t, Data_axis, linewidth=0.5)
     plt.ylabel('Amplitude du signal')
     plt.xlabel('Temps')
 
-    plt.subplot(212) #subplot pour le graphique fréquentiel
-    plt.plot(xf,2*MOY/len(MOY))
+    plt.subplot(212)  # subplot pour le graphique fréquentiel
+    plt.plot(xf, 2 * MOY / len(MOY))
     plt.ylabel('Amplitude de la FFT')
     plt.xlabel('Fréquence')
     axes = plt.gca()
     axes.set_xlim([0, 25])
     plt.grid(True)
     plt.savefig('Images/Analyse-capt-' + Axe + '.png')
+
+
 #########################################
 def affiche():
     # fonction servant a envoyer le bon caractère sur le port série en fonction du choix des checkbox sur le GUI
@@ -204,29 +229,44 @@ def affiche():
         choix = "E"
     elif arg == 5:
         choix = "F"
-    #print(choix)
+    # print(choix)
     return choix
+
+
 ######################################### MODIF
 def start():
-    global rapport # définition d'une variable globale pour empêcher l'utilisation du bouton imprimer avant d'avoir réalisé l'analyse.
+    global rapport  # définition d'une variable globale pour empêcher l'utilisation du bouton imprimer avant d'avoir réalisé l'analyse.
     bouton_print.configure(state="disabled")
+    choix = affiche()  # fonction permettant d'envoyer le bon caractère sur le port série pour définir le temps d'acquisition du programme.
+    Nb_ligne, Nb_ech = define_value(
+        choix)  # fonction permettant de définir les constantes utilisées dans le code en fonction du choix de l'utilisateur.
     bouton_start.configure(state="disabled")
-    choix = affiche() # fonction permettant d'envoyer le bon caractère sur le port série pour définir le temps d'acquisition du programme.
-    Nb_ligne, Nb_ech = define_value(choix) # fonction permettant de définir les constantes utilisées dans le code en fonction du choix de l'utilisateur.
-    Datas = read_serial(Nb_ligne) # Lecture des données envoyées par l'arduino sur le port série et enregistrement des données dans une liste.
-    axis_split = [] # définition d'une liste pour les époques de 2 secondes
-    for k in range(0,3*nbcapteur):    # boucle for allant de 0 jusqu'au nombre d'axe*nombre de capteurs ( 1 capteur * 3 axes)
-        Data_Axis = get_axis_value(k,Datas,Nb_ligne) # fonction utilisée pour changer la taille de la matrice Datas pour récuperer les valeurs pour chaque axe
-        axis_split = [Data_Axis[x:x+2*Fe] for x in range(0,len(Data_Axis),2*Fe)] # Split de la liste des valeurs pour un axe en époque de 2 secondes( 2*Fe )
-        plot(k,Nb_ligne,Data_Axis,axis_split) # fonction servant à generer nos axes
-    rapport = True # autorisation d'utilisation du bouton d'impression
+    fenetre.update_idletasks()
+    info_label.grid(row=4, column=0, sticky='w', padx=3, columnspan=2)
+    info_label['text'] = "La prise de mesure de {} secondes a démarré.                 ".format(30 * (Nb_ligne // 3000))
+    fenetre.update_idletasks()
+    Datas = read_serial(
+        Nb_ligne)  # Lecture des données envoyées par l'arduino sur le port série et enregistrement des données dans une liste.
+    axis_split = []  # définition d'une liste pour les époques de 2 secondes
+    for k in range(0,
+                   3 * nbcapteur):  # boucle for allant de 0 jusqu'au nombre d'axe*nombre de capteurs ( 1 capteur * 3 axes)
+        Data_Axis = get_axis_value(k, Datas,
+                                   Nb_ligne)  # fonction utilisée pour changer la taille de la matrice Datas pour récuperer les valeurs pour chaque axe
+        axis_split = [Data_Axis[x:x + 2 * Fe] for x in range(0, len(Data_Axis),
+                                                             2 * Fe)]  # Split de la liste des valeurs pour un axe en époque de 2 secondes( 2*Fe )
+        plot(k, Nb_ligne, Data_Axis, axis_split)  # fonction servant à generer nos axes
+    rapport = True  # autorisation d'utilisation du bouton d'impression
     bouton_print.configure(state="normal")
     bouton_start.configure(state="normal")
     return axis_split
+
+
 #########################################
 def rap_gen():
     # fonction utilisé pour pouvoir generer un rapport pdf grace au bouton imprimer
     rapport_gen(var_nom.get(), var_prenom.get(), var_date.get())
+
+
 #########################################
 def on_entry_click(event):
     # fonction permettant de vider les champs de texte en appuyant sur la fenêtre
@@ -238,13 +278,20 @@ def on_entry_click(event):
         entry2.delete(0, "end")
         entry3.delete(0, "end")
 
+
 def ifcompleted(*args):
     if ((var_nom.get() != entry1_def_msg and len(var_nom.get()) > 0) and  # nom inchangé ou vide
             (var_prenom.get() != entry2_def_msg and len(var_prenom.get()) > 0) and  # prenom inchangé ou vide
             (var_date.get() != entry3_def_msg and len(var_date.get()) > 0)):  # date inchangée ou vide
         bouton_start.configure(state="normal")
+        info_label['text'] = "Appuyez sur démarrer pour commencer l'analyse."
+        fenetre.update_idletasks()
     else:
         bouton_start.configure(state="disabled")
+        info_label['text'] = "Veuillez remplir tous les champs."
+        fenetre.update_idletasks()
+
+
 #########################################
 fenetre = Tk()
 #########################################
@@ -252,7 +299,6 @@ fenetre = Tk()
 var_nom = StringVar()
 var_prenom = StringVar()
 var_date = StringVar()
-var_state = "Appuyez sur démarrer pour commencer l'analyse"
 fenetre.title("Tremor Measurement")
 #########################################
 # le code suivant permet la création de boutons, texte d'entrée et checkbox que nous pouvons placer a notre guise.
@@ -263,42 +309,50 @@ fenetre.title("Tremor Measurement")
 var_case = IntVar()
 cb = []
 
-label = Label(fenetre, text="Nom du patient: ")
+label = Label(fenetre, text="Nom du patient : ")
 label.grid(row=0, column=0, sticky='w')
 
 entry1 = Entry(fenetre, bd=1, width=33, textvariable=var_nom)
-entry1_def_msg='Veuillez rentrer le nom du patient:'
+entry1_def_msg = 'Veuillez rentrer le nom du patient:'
 entry1.insert(0, entry1_def_msg)
 entry1.bind('<FocusIn>', on_entry_click)
 entry1.grid(row=0, column=1, sticky='w')
 var_nom.trace("w", ifcompleted)
 
-
-label = Label(fenetre, text="Prénom du patient: ")
+label = Label(fenetre, text="Prénom du patient : ")
 label.grid(row=1, column=0, sticky='w')
 
 entry2 = Entry(fenetre, bd=1, width=33, textvariable=var_prenom)
-entry2_def_msg='Veuillez rentrer le prénom du patient:'
+entry2_def_msg = 'Veuillez rentrer le prénom du patient:'
 entry2.insert(0, entry2_def_msg)
 entry2.bind('<FocusIn>', on_entry_click)
 entry2.grid(row=1, column=1, sticky='w')
 var_prenom.trace("w", ifcompleted)
 
-label = Label(fenetre, text="Date de naissance du patient:")
+label = Label(fenetre, text="Date de naissance du patient : ")
 label.grid(row=2, column=0, sticky='w')
-
 entry3 = Entry(fenetre, bd=1, width=33, textvariable=var_date)
-entry3_def_msg='JJ/MM/AAAA'
+entry3_def_msg = 'JJ/MM/AAAA'
 entry3.insert(0, entry3_def_msg)
 entry3.bind('<FocusIn>', on_entry_click)
 entry3.grid(row=2, column=1, sticky='w')
 var_date.trace("w", ifcompleted)
 
+info_msg = "Complétez les informations sur le patient avant de démarrer."
+info_label = Label(fenetre, text=info_msg)
+info_label.grid(row=4, column=0, sticky='w', padx=3, columnspan=2)
+
+info2_label = Label(fenetre)
+info2_label.grid(row=5, column=0, sticky='w', padx=3, columnspan=2)
+
 for i in range(6):
     cb.append(Checkbutton(fenetre, text=str((i + 1) * 30) + " Secondes", onvalue=i, variable=var_case))
     # création automatique des checkbox. pour en générer plus ou moins il suffit de changer le paramètre présent dans le
     # range.
-    cb[i].grid(row=i, column=2)
+    cb[i].grid(row=i, column=2, sticky='w', padx=10)
+
+decounter = tk.Label(fenetre)
+decounter.grid(row=4, column=0, sticky='w')
 
 bouton_start = Button(fenetre, text="Démarrer", command=start)
 bouton_start.configure(state="disabled")
@@ -308,9 +362,7 @@ bouton_stop = Button(fenetre, text="Annuler", command=fenetre.quit)
 bouton_stop.grid(row=6, column=1, sticky='w', padx=17)
 bouton_print = Button(fenetre, text="Imprimer le rapport", command=rap_gen)
 bouton_print.configure(state="disabled")
-progress = Progressbar(fenetre, length = 100, mode = 'determinate')
-progress.grid(row=6, column=2, sticky='w', padx=15)
-bouton_print.grid(row=12, column=2, sticky='s', pady=10)
+bouton_print.grid(row=12, column=2, sticky='s', pady=10, padx=10)
 # On démarre la boucle Tkinter qui s'interompt quand on ferme la fenêtre
 fenetre.mainloop()
 #########################################
